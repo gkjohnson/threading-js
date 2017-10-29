adc2869cc877830fcb3897ef09b4f6f8fad66ae0
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-10-29 10:24:43 -0700

    comments

--- ThreadPool.js
+++ ThreadPool.js
@@ -1,6 +1,10 @@
+// Thread Pool for creating multiple threads with the same
+// function and running them in parallel
 class ThreadPool {
 
+    // whether the pool has available threads
     get ready() { return this._activeThreads < this._capacity }
+
     get activeThreads() { return this._activeThreads }
     get capacity() { return this._capacity }
     
@@ -13,11 +17,18 @@ class ThreadPool {
     }
 
     run() {
-        this._activeThreads = Math.max(this._activeThreads + 1, this._capacity)
-
+        // Increment the number of running threads up to
+        // capacity.
+        this._activeThreads = Math.min(this._activeThreads + 1, this._capacity)
         if (this._activeThreads > this._threads.length) this._threads.push(new Thread(...this._threadArgs))
 
-        const currThread = this._threads.filter(t => t.ready && !t.running)[0]
+        // Find a thread to run. If we can't find a thread that is
+        // ready and able to run, we return null
+
+        // TODO: Make sure the thread will queue up a run even if it's not
+        // ready yet so we don't have this weird async issue
+        const currThread = this._threads.filter(t => !t.running)[0]
+        console.log(this._threads.filter(t => !t.running))
         if (!currThread) return null
         
         return new Promise((res, rej) => {
@@ -42,21 +53,22 @@ class ThreadPool {
     }
 }
 
+// Class to enqueue jobs and run across multiple threads
 class ThreadQueue extends ThreadPool {
     get ready() { return true }
 
     run() {
         this._queue = this._queue || []
-        const job = {
-            args: [...arguments],
-            promise: null
-        }
+
+        // create the job to enqueue
+        const job = { args: [...arguments], promise: null }
         const pr = new Promise((resolve, reject) => job.promise = { resolve, reject })
 
         this._queue.push(job)
         this.tryRunQueue()
     }
 
+    // Try to run the jobs on the queue
     tryRunQueue() {
         while (this.ready && this._queue.length) {
             const job = this._queue.shift()
