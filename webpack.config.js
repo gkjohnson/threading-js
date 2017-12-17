76c15819b6a3621a3dbe584372bf7c5aa57784b5
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-12-17 11:44:27 -0800

    Fix the webpack file to properly return an array

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,40 @@
-// This webpack config is built to convert a particular file
-// specified in the PACKAGE environment variable into a UMD
-// library file from the es6 import syntax:
-
-// cross-env PACKAGE=ThreadQueue webpack
-
-// will build ThreadQueue.js into a single file without bundling
-// the Thread.js and ThreadPool.js dependencies. These means that
-// all the files need to be built with separate processes
-
-const libraryName = process.env.PACKAGE
-const fileName = `${process.env.PACKAGE}.js`
-
-// generate the list of external files and how to import them
-// for each import convention
+// Build all three files independently
 const packages = ['Thread', 'ThreadPool', 'ThreadQueue']
-const externals = {}
-packages.forEach(pkgName => {
-    if (pkgName !== libraryName) {
-        const file = `./${pkgName}.js`
-        externals[file] = {
-            commonjs2:  file,
-            commonjs:   file,
-            amd:        file,
-            root:       pkgName
-        }
-    }
-})
 
-module.exports = {
-    entry: `./${fileName}`,
+module.exports = 
+    packages
+        .map(pkgName => {
+            // This packages library name and
+            // the file name
+            const libraryName = pkgName
+            const fileName = `${pkgName}.js`
 
-    // Target the same file destination but in the UMD directory
-    // with a target format of UMD
-    output: {
-        filename:       `./umd/${fileName}`,
-        library:        libraryName,
-        libraryTarget:  'umd'
-    },
+            // Push the other packages into the
+            // externals object
+            const externals = {}
+            packages
+                .filter(p => pkgName !== p)
+                .forEach(pkgName => {
+                    const file = `./${pkgName}.js`
+                    externals[file] = {
+                        commonjs2:  file,
+                        commonjs:   file,
+                        amd:        file,
+                        root:       pkgName
+                    }
+                })
 
-    externals
-}
+            return {
+                entry: `./${fileName}`,
+
+                // Target the same file destination but in the UMD directory
+                // with a target format of UMD
+                output: {
+                    filename:       `./umd/${fileName}`,
+                    library:        libraryName,
+                    libraryTarget:  'umd'
+                },
+
+                externals
+            }
+        })
